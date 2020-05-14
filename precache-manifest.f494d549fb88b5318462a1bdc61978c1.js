5752890394510a2c5ee0f3de6f2a39372c196603
Niksa <nhribljan@gmail.com>
2020-05-14 19:28:41 +0200

    Updates

---
+++ precache-manifest.f494d549fb88b5318462a1bdc61978c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d48832df8598f274e072a20c981e0b45",
+    "url": "/covid-19-app/index.html"
+  },
+  {
+    "revision": "9c41cc4e7b42b2b2d988",
+    "url": "/covid-19-app/static/css/main.0dde928b.chunk.css"
+  },
+  {
+    "revision": "53376489028174fad310",
+    "url": "/covid-19-app/static/js/2.ab360487.chunk.js"
+  },
+  {
+    "revision": "0f0b7b8d3d7295a2b7b92a69b1e8eb96",
+    "url": "/covid-19-app/static/js/2.ab360487.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c41cc4e7b42b2b2d988",
+    "url": "/covid-19-app/static/js/main.e0ce69c8.chunk.js"
+  },
+  {
+    "revision": "6d4d886789d2beeb45ee",
+    "url": "/covid-19-app/static/js/runtime-main.c6af11ba.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-app/static/media/image.d7265326.png"
+  }
+]);
